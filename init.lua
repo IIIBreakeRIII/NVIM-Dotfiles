488f3d66f8b0a6a3a2bf1c7eb90ae5995a32716c
IIIBreakeRIII <ryu990305@icloud.com>
2025-07-13 21:28:18 +0900

    Add Python Debug Key Bind

--- init.lua
+++ init.lua
@@ -4,3 +4,4 @@ require('vim-setup.key-bind')
 require('lazy-setup.lazy')
 
 vim.api.nvim_command([[autocmd FileType c nnoremap <buffer> <F5> :term gcc % && ./a.out<CR>]])
+vim.api.nvim_command([[autocmd FileType python noremap <buffer> <F5> :term python3 %<CR>]])
